4779ab1aa9f63bc02f9926f46d236dd5eedd270d
JosephObote <joeobote01@gmail.com>
2023-09-12 21:36:54 +0300

    completed assignment

---
+++ functions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print("Hello, Programmer!")
+
+    
+
+def greet(name):
+    print(f"Hello,{name}!")
+
+
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}")
+greet_with_default()
+    
+
+def add(num1, num2):
+    sum_result =num1+num2
+    return sum_result
+add(30, 40)
+def halve(number):
+    pass
